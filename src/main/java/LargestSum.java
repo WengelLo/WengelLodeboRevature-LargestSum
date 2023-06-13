4c7d9e2c9004f9d49e74a8cb029cdce3dd2f6c0f
Wengel Lodebo <tes.wengel@gmail.com>
2023-06-13 02:39:06 +0000

    5

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,25 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                int num1 = nums.get(i);
+                int num2 = nums.get(j);
+                if (num1 != num2) {
+                    int sum = num1 + num2;
+                    if (sum > answer) {
+                        answer = sum;
+                    }
+                } else {
+                    int sum = num1 * 2;
+                    if (sum > answer) {
+                        answer = sum;
+                    }
+                }
+            }
+        }
+        return answer;
+        
     }
 }
